3fe6ea35028cae133cd9a8ccf19dfa0462640fa0
Mahmoud Salah <Mahmoudsalah20300@gmail.com>
2023-12-29 14:10:31 +0200

    #

--- Data_Structure_Represention_In_C++/Linked_List.h
+++ Data_Structure_Represention_In_C++/Linked_List.h
@@ -35,10 +35,25 @@ class Linked_List {
 
 
 template<typename T>
-class Double_Node
+class Double_Node:public Node<T>
 {
 	
+	public:
+	Double_Node* prev;
+	Double_Node(T);
 };
 
+
+
+
+template<typename T>
+class Double_Linked_List {
+
+	private:
+		Double_Node<T>* Tail;
+	public:
+		Double_Linked_List();
+		~Double_Linked_List();
+};
 
 
