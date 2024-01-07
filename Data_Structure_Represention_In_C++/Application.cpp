4c202aad340b9d885c6884e6df390eedc7ef1a5e
Mahmoud Salah <Mahmoudsalah20300@gmail.com>
2024-01-07 09:12:25 +0200

    Modefing the implementation of Queue to make Queue size is fixed as the User set in the beginning.

--- Data_Structure_Represention_In_C++/Application.cpp
+++ Data_Structure_Represention_In_C++/Application.cpp
@@ -1,12 +1,27 @@
 #include"Linked_List_testing.h"
 #include"Double_Linked_List.h"
 #include"Stack_Testing.h"
-
+#include"Queue.h"
 
 
 int main()
 {
-	
-
+	Queue<int> Mahmoud;
+	Mahmoud.Get_Queue_Size(5);
+	Mahmoud.EnQueue(10);
+	//Mahmoud.Display_Elements();
+	Mahmoud.EnQueue(15);
+	//Mahmoud.Display_Elements();
+	Mahmoud.EnQueue(16);
+	//Mahmoud.Display_Elements();
+	Mahmoud.EnQueue(17);
+	//Mahmoud.Display_Elements();
+	Mahmoud.EnQueue(18);
+	//Mahmoud.Display_Elements();
+	Mahmoud.EnQueue(19);
+	Mahmoud.Display_Elements();
+	Mahmoud.EnQueue(-19);
+	Mahmoud.Display_Elements();
+	//std::cout << Place_Taken << "\t";
 
 }
--- Data_Structure_Represention_In_C++/Queue.h
+++ Data_Structure_Represention_In_C++/Queue.h
@@ -3,14 +3,14 @@
 
 #include"Linked_List.h"
 
-static int Place_Taken = 0;
+int Place_Taken = 1;
 
 template<typename T>
 class Queue {
 	private:
 		using New_Node = Node<T>;
-		Node* head;
-		T* tail;
+		New_Node* head;
+		New_Node* tail;
 		int SIZE = 0;
 	public:
 		Queue();
@@ -42,30 +42,67 @@ void Queue<T>::Get_Queue_Size(int value)
 	SIZE = value;
 }
 
-/* Need Modification*/
+
+
 template<typename T>
 void Queue<T>::EnQueue(T value)
 {	
-	New_Node* turn = new New_Node(value);
-	New_Node* Current = head;
+	if (SIZE > 0  ) {
+		New_Node* turn = new New_Node(value);
+		New_Node* Current = head;
 
-	if (Place_Taken != SIZE) {
-		if (head)
-		{
-			turn->next = head;
-			head = turn;
+		if (Place_Taken <= SIZE) {
+			++Place_Taken;
+			if (head)
+			{
+				turn->next = head;
+				head = turn;
+			}
+			else
+			{
+				head = turn;
+			}
+			
+			std::cout << "\nI am in 65 and Places is " << Place_Taken;
 		}
 		else
 		{
+			
+			while (Current->next->next)
+			{	
+				
+				Current = Current->next;
+				
+			}
+			
+			tail = Current;
+			turn->next = head;
 			head = turn;
-		}
-		++Place_Taken;
-	}
-	else
-	{
-		while (Current)
-		{
 			Current = Current->next;
+			delete Current;
+			tail->next = nullptr;
+
 		}
+		
+		Current = nullptr;
+		delete Current;
+
 	}
+	else{
+		std::cout << "\nError: The Size of the Queue Must be bigger than 0\n";
+	}
+}
+
+template<typename T>
+void Queue<T>::Display_Elements() {
+	
+	New_Node* Current = head;
+	std::cout << "\nThe Current Elments in Queue:\n";
+	while (Current)
+	{
+		std::cout << Current->data <<"\t";
+		Current = Current->next;
+	}
+	Current = nullptr;
+	delete Current;
 }
